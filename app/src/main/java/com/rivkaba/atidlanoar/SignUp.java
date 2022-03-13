d442cf2c5782f2b80e9bc33def46ae03a5576e87
Rivka <rbir359718@gmail.com>
2022-03-13 19:30:00 +0200

    connects to firebase

--- app/src/main/java/com/rivkaba/atidlanoar/SignUp.java
+++ app/src/main/java/com/rivkaba/atidlanoar/SignUp.java
@@ -2,13 +2,31 @@ package com.rivkaba.atidlanoar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
-
+    public FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            startActivity(new Intent(SignUp.this,Student.class));
+        }
+    }
+
+    public void register(View view) {
     }
 }

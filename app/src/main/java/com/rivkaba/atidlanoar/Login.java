b6d5561ff8654d2ce1843558b48a0b37dce9cab8
Rivka <rbir359718@gmail.com>
2022-03-16 16:07:17 +0200

    class user

--- app/src/main/java/com/rivkaba/atidlanoar/Login.java
+++ app/src/main/java/com/rivkaba/atidlanoar/Login.java
@@ -1,20 +1,59 @@
 package com.rivkaba.atidlanoar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
-
+    public FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            startActivity(new Intent(Login.this,Student .class));
+        }
+    }
+        public void login(View view) {
+            EditText email=findViewById(R.id.editEmail);
+            EditText password =findViewById(R.id.editPassword);
 
-    public void student(View view) {
-        startActivity(new Intent(Login.this,Student.class));
+            mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                startActivity(new Intent(Login.this,Student.class));
+                          /*  Intent intent = new Intent(LoginActivity.this, profile.class);
+                            intent.putExtra("mAuth", FirebaseAuth.getInstance());
+                            startActivity(intent);*/
+                            } else {
+                                Toast.makeText(Login.this,"כניסה נכשלה:)",Toast.LENGTH_LONG).show();
+                                // If sign in fails, display a message to the user.
+
+                            }
+                        }
+
+                    });
+
+            }
     }
-}
--- app/src/main/java/com/rivkaba/atidlanoar/SignUp.java
+++ app/src/main/java/com/rivkaba/atidlanoar/SignUp.java
@@ -1,11 +1,17 @@
 package com.rivkaba.atidlanoar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -17,16 +23,25 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
         mAuth = FirebaseAuth.getInstance();
     }
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            startActivity(new Intent(SignUp.this,Student.class));
-        }
-    }
 
     public void register(View view) {
+        EditText email=findViewById(R.id.editTextEmail);
+        EditText password =findViewById(R.id.editPhone);
+        mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignUp.this," בתור התחלה מס' הפלאפון ישמש כסיסמה",Toast.LENGTH_LONG).show();
+                            // Sign in success, upda
+                            // te UI with the signed-in user's information
+
+                        } else {
+                            Toast.makeText(SignUp.this,"ההרשמה נכשלה:)",Toast.LENGTH_LONG).show();
+                            // If sign in fails, display a message to the user.
+
+                        }
+                    }
+                });
     }
 }

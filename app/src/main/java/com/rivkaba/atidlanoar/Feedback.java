79fc5d23539af11dbac0aa1292d210717a51928c
Rivka <rbir359718@gmail.com>
2022-07-12 19:23:07 +0300

    feedback seve data

--- app/src/main/java/com/rivkaba/atidlanoar/Feedback.java
+++ app/src/main/java/com/rivkaba/atidlanoar/Feedback.java
@@ -1,5 +1,6 @@
 package com.rivkaba.atidlanoar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,16 +10,27 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class Feedback extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Feedback extends AppCompatActivity  {
+    String uid;
+    EditText editTextDate;
+    public FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        db = FirebaseFirestore.getInstance();
 
     }
 
@@ -71,92 +83,41 @@ public class Feedback extends AppCompatActivity {
         startActivity(new Intent(Feedback.this,Student.class));
     }
 
+    public void send(View view) {
+        editTextDate =findViewById(R.id.editTextDate);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Check if user's email is verified
+          //  boolean emailVerified = user.isEmailVerified();
+             uid = user.getUid();
+        }
+        String date1 = editTextDate.getText().toString();
+//////////
+        Map<String, Object> docData = new HashMap<>();
+        docData.put("approved", true);
+        docData.put("form", "form");
+        docData.put("date", date1);
+        db.collection("students").document(uid).collection("comes").document(date1).set(docData)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(Feedback.this, " תודה, הטופס נשלח בהצלחה", Toast.LENGTH_LONG).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Feedback.this, "השליחה נכשלה", Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+        ///////////////
 
 
-//    @Override
-//    public boolean onCreateOptionsMenu(final Menu menu) {
-//        super.onCreateOptionsMenu(menu);
-//
-//        MenuItem privateState = menu.add("Call to manegment");
-//        MenuItem exitMenu = menu.add("Exit");
-//
-//        privateState.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-//            @Override
-//            public boolean onMenuItemClick(MenuItem item) {
-//                CallToManegment();
-//                return false;
-//            }
-//        });
-//
-//        exitMenu.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-//            @Override
-//            public boolean onMenuItemClick(MenuItem item) {
-//                exitApp();
-//                return false;
-//            }
-//        });
-//
-//        return true;
-//    }
-//
-//    private void exitApp() {
-//        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-//        alertDialog.setTitle("Exit ");
-//        alertDialog.setMessage("Do you want to exit ?");
-//        alertDialog.setCancelable(false);
-//        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener()
-//            @Override
-//            public void onClick (DialogInterface dialog,int which){
-//                FirebaseAuth.getInstance().signOut();
-//                Toast.makeText(Feedback.this, "Bye Bye ...", Toast.LENGTH_LONG).show();
-//                finish();  // destroy this activity
-//                //System.exit(0);
-//                //  android.os.Process.killProcess(android.os.Process.myPid());
-//                moveTaskToBack(true);
-//
-//
-//            }
-//        });
-//        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                dialog.dismiss(); // close the dialog
-//            }
-//        });
-//        alertDialog.show();
-//    }
-//
-//    private void CallToManegment() {
-//        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-//        alertDialog.setTitle("Exit ");
-//        alertDialog.setMessage("Do you want to Call to manegment ?");
-//        alertDialog.setCancelable(false);
-//        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                Intent intent = new Intent(Feedback.this, Login.class);
-//                startActivity(intent);
-//                //  android.os.Process.killProcess(android.os.Process.myPid());
-//                //moveTaskToBack(true);
-//
-//            }
-//        });
-//        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                dialog.dismiss(); // close the dialog
-//            }
-//        });
-//        alertDialog.show();
-//        singIn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(Feedback.this, Login.class);
-//                startActivity(intent);
-//                finish();
-//            }
-//
-//        });
+    }
 
- //   }
+
+
 }

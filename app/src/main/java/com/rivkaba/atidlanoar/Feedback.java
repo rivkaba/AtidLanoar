242e2fb8cd779e3dc42585fcba8f779192719fc0
Rivka <rbir359718@gmail.com>
2022-07-17 16:15:51 +0300

    feedback save information

--- app/src/main/java/com/rivkaba/atidlanoar/Feedback.java
+++ app/src/main/java/com/rivkaba/atidlanoar/Feedback.java
@@ -11,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -23,8 +24,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Feedback extends AppCompatActivity  {
+
     String uid;
     EditText editTextDate;
+    EditText feedback1;
+    String q1;
+    String q2;
+    String q3;
+    String q4;
+    boolean canUpdate=false;
     public FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,26 +88,14 @@ public class Feedback extends AppCompatActivity  {
     }
     // end Menu
     public void back(View view) {
+
         startActivity(new Intent(Feedback.this,Student.class));
     }
-//**********
-//form
-//        canUpdate
-//false
-//    feedback
-//"אני התערבתי עם אנשים אאף פעם לא ראיתי אותם זה יעזור לי להתמדד בעתיד אם אנשים. "
-//    feeedbackMeeting
-//            q1
-//"0"
-//    q2
-//"1"
-//    q3
-//"2"
-//    q4
-//"3"
-    //**********
+
     public void send(View view) {
         editTextDate =findViewById(R.id.editTextDate);
+        feedback1=findViewById(R.id.feedback);
+        String feedback = feedback1.getText().toString();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             // Check if user's email is verified
@@ -107,16 +103,23 @@ public class Feedback extends AppCompatActivity  {
              uid = user.getUid();
         }
         String date1 = editTextDate.getText().toString();
-//////////
+       // canUpdate
         Map<String, Object> docData = new HashMap<>();
         docData.put("approved", true);
         docData.put("date", date1);
 
 
         Map<String, Object> form = new HashMap<>();
-        form.put("a", 5);
-        form.put("b", true);
+        form.put("canUpdate", canUpdate);
+        form.put("feedback", feedback);
 
+        Map<String, Object> feeedbackMeeting = new HashMap<>();
+        feeedbackMeeting.put("q1", q1);
+        feeedbackMeeting.put("q2", q2);
+        feeedbackMeeting.put("q3", q3);
+        feeedbackMeeting.put("q4", q4);
+
+        form.put("feeedbackMeeting", feeedbackMeeting);
         docData.put("from", form);
 
         db.collection("students").document(uid).collection("comes").document(date1).set(docData)
@@ -139,7 +142,117 @@ public class Feedback extends AppCompatActivity  {
 
 
     }
+    //q1
+    public void onRadioButtonClicked1(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio10:
+                if (checked)
+                   q1="0";
+                    break;
+            case R.id.radio11:
+                if (checked)
+                    q1="1";
+                    break;
+            case R.id.radio12:
+                if (checked)
+                    q1="2";
+                    break;
+            case R.id.radio13:
+                if (checked)
+                    q1="3";
+                break;
+            case R.id.radio14:
+                if (checked)
+                    q1="4";
+                break;
+        }
+    }
+//q2
 
-
-
+    public void onRadioButtonClicked2(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio20:
+                if (checked)
+                    q2="0";
+                break;
+            case R.id.radio21:
+                if (checked)
+                    q2="1";
+                break;
+            case R.id.radio22:
+                if (checked)
+                    q2="2";
+                break;
+            case R.id.radio23:
+                if (checked)
+                    q2="3";
+                break;
+            case R.id.radio24:
+                if (checked)
+                    q2="4";
+                break;
+        }
+    }
+//q3
+    public void onRadioButtonClicked3(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio30:
+                if (checked)
+                    q3="0";
+                break;
+            case R.id.radio31:
+                if (checked)
+                    q3="1";
+                break;
+            case R.id.radio32:
+                if (checked)
+                    q3="2";
+                break;
+            case R.id.radio33:
+                if (checked)
+                    q3="3";
+                break;
+            case R.id.radio34:
+                if (checked)
+                    q3="4";
+                break;
+        }
+    }
+//q4
+    public void onRadioButtonClicked4(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio40:
+                if (checked)
+                    q4="0";
+                break;
+            case R.id.radio41:
+                if (checked)
+                    q4="1";
+                break;
+            case R.id.radio42:
+                if (checked)
+                    q4="2";
+                break;
+            case R.id.radio43:
+                if (checked)
+                    q4="3";
+                break;
+            case R.id.radio44:
+                if (checked)
+                    q4="4";
+                break;
+        }
+    }
 }

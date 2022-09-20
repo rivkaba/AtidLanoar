718057fdf621346a2fd06f53e5c1e8bbb4c326ae
Rivka <rbir359718@gmail.com>
2022-09-20 12:48:32 +0300

    Summary questionnaire  send data

--- app/src/main/java/com/rivkaba/atidlanoar/Summary_questionnaire.java
+++ app/src/main/java/com/rivkaba/atidlanoar/Summary_questionnaire.java
@@ -10,8 +10,10 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.Spinner;
@@ -19,19 +21,34 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class Summary_questionnaire extends AppCompatActivity {
     public FirebaseFirestore db;
+    String uid;
+    String q1;
     ArrayList<String> team = new ArrayList<String>();
-    private TextView textView;
+    private TextView age;
     private SeekBar seekBar;
+    private TextView gender;
+    private TextView agee;
+    private RadioButton q110;
+    private RadioButton q111;
+    private RadioButton q112;
+    private EditText q113;
+    boolean ch = false;
     private Button questionnair;
     private ScrollView scrollView;
     private ScrollView scrollView1;
@@ -46,6 +63,12 @@ public class Summary_questionnaire extends AppCompatActivity {
         scrollView=(ScrollView) findViewById(R.id.Scroll_view_questionnaire);
         scrollView1=(ScrollView) findViewById(R.id.Scroll_view_questionnaire_1);
         linearButtons =(LinearLayout) findViewById(R.id.linear_buttons);
+        gender=(TextView) findViewById(R.id.gender);
+        agee=(TextView) findViewById(R.id.agee);
+        q110=(RadioButton) findViewById(R.id.q110);
+        q111 = (RadioButton) findViewById(R.id.q111);
+        q112 = (RadioButton) findViewById(R.id.q112);
+        q113 = (EditText) findViewById(R.id.q113);
         Intent intent = getIntent();
         String part = intent.getStringExtra("part");
         if(Objects.equals(part, "P1"))
@@ -92,13 +115,14 @@ public class Summary_questionnaire extends AppCompatActivity {
             }
         });
         // seekBar age
-        textView = (TextView) findViewById(R.id.age);
+        age = (TextView) findViewById(R.id.age);
         seekBar = (SeekBar) findViewById(R.id.q11);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-                textView.setText(String.valueOf(progress));
+                agee.setText(String.valueOf(progress));
+                ch=true;
             }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
@@ -153,7 +177,107 @@ public class Summary_questionnaire extends AppCompatActivity {
 
     }
 
-    public void send(View view) {
 
+    //q1 gender
+    public void onRadioButtonClicked1(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.q110:
+                if (checked)
+                    q1 = "זכר";
+                break;
+            case R.id.q111:
+                if (checked)
+                    q1 = "נקבה";
+                break;
+            case R.id.q112://אחר
+                if (checked)
+                    q113.setVisibility(view.VISIBLE);
+                q1 = q113.getText().toString();
+                break;
+
+        }
+        }
+
+    public void save(View view) {
+        //fulling
+        if ((!q110.isChecked()) && (!q111.isChecked()) && (!q112.isChecked())) {
+            gender.setTextColor(getResources().getColor(R.color.design_default_color_error));
+        }
+        if (!ch) {
+            age.setTextColor(getResources().getColor(R.color.design_default_color_error));
+        }
+//                if (teamm.getText().toString().equals("")) {
+//                    teamm.setHintTextColor(getResources().getColor(R.color.design_default_color_error));
+//                }
+
+//&&(send2()==1)
+        if (((q110.isChecked()) || (q111.isChecked()) || (q112.isChecked())) && (ch)) {
+
+            gender.setTextColor(getResources().getColor(R.color.black));
+            agee.setTextColor(getResources().getColor(R.color.black));
+            Map<String, Object> answers = new HashMap<>();
+            Map<String, Object> part1 = new HashMap<>();
+            part1.put("gender", q1);
+            part1.put("age", age.getText().toString());
+//                //  part1.put("teamName", teamName);
+//                Map<String, Object> part2 = new HashMap<>();
+//                part2.put("q1", qq21);
+//                part2.put("q2", qq22);
+//                part2.put("q3", qq23);
+//                part2.put("q4", qq24);
+//                part2.put("q5", qq25);
+//                part2.put("q6", qq26);
+//                part2.put("q7", qq27);
+//                part2.put("q8", qq28);
+//                part2.put("q9", qq29);
+//                part2.put("q10", qq210);
+//                part2.put("q11", qq211);
+//                part2.put("q12", qq212);
+
+
+            //    Map<String, Object> part3 = new HashMap<>();
+//        Q31=String.valueOf(q31.getRating());
+//        Q32=String.valueOf(q32.getRating());
+//        Q33=String.valueOf(q33.getRating());
+//        Q34=String.valueOf(q34.getRating());
+//
+//        part3.put("q31", Q31);
+//        part3.put("q32", Q32);
+//        part3.put("q33", Q33);
+//        part3.put("q34", Q34);
+
+            //        Map<String, Object> part4 = new HashMap<>();
+//        String Q41 = q41.getText().toString();
+//        String Q42 = q42.getText().toString();
+//        part4.put("q41", Q41);
+//        part4.put("q42", Q42);
+
+            answers.put("part1", part1);
+            //answers.put("part2", part2);
+//                answers.put("part3", part3);
+//                answers.put("part4", part4);
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if (user != null) {
+                uid = user.getUid();
+            }
+            db.collection("students").document(uid).collection("questionnaires").document("Summary questionnaire").set(answers)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(Summary_questionnaire.this, " תודה, הטופס נשלח בהצלחה", Toast.LENGTH_LONG).show();
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(Summary_questionnaire.this, "השליחה נכשלה", Toast.LENGTH_LONG).show();
+
+                        }
+                    });
+        }
     }
-}
+    }
--- app/src/main/java/com/rivkaba/atidlanoar/Summary_questionnaire_3.java
+++ app/src/main/java/com/rivkaba/atidlanoar/Summary_questionnaire_3.java
@@ -2,6 +2,7 @@ package com.rivkaba.atidlanoar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewTreeViewModelKt;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.widget.RadioButton;
 import android.widget.RatingBar;
 import android.widget.ScrollView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -46,7 +48,46 @@ public class Summary_questionnaire_3 extends AppCompatActivity {
     SeekBar   q46;
     EditText q47;
     EditText q48;
-
+    int Q45 ;
+    int Q46 ;
+    String Q47;
+    String Q48;
+    TextView qq31;
+    TextView qq32;
+    TextView qq33;
+    TextView qq34;
+    TextView qq45;
+    TextView qq46;
+    TextView qq47;
+    TextView qq48;
+    private RadioButton q440;
+    private RadioButton q441;
+    private RadioButton q442;
+    private RadioButton q443;
+    private RadioButton q444;
+    private RadioButton q445;
+    private RadioButton q430;
+    private RadioButton q431;
+    private RadioButton q432;
+    private RadioButton q433;
+    private RadioButton q434;
+    private RadioButton q435;
+    private RadioButton q420;
+    private RadioButton q421;
+    private RadioButton q422;
+    private RadioButton q423;
+    private RadioButton q424;
+    private RadioButton q425;
+    private RadioButton q411;
+    private RadioButton q410;
+    private RadioButton q412;
+    private RadioButton q413;
+    private RadioButton q414;
+    private RadioButton q415;
+    TextView qq41;
+    TextView qq42;
+    TextView qq43;
+    TextView qq44;
     String uid;
     public FirebaseFirestore db;
 
@@ -66,10 +107,46 @@ public class Summary_questionnaire_3 extends AppCompatActivity {
         q32=(RatingBar)findViewById(R.id.q32);
         q33=(RatingBar)findViewById(R.id.q33);
         q34=(RatingBar)findViewById(R.id.q34);
+        q440=(RadioButton)findViewById(R.id.q440);
+        q441=(RadioButton)findViewById(R.id.q441);
+        q442=(RadioButton)findViewById(R.id.q442);
+        q443=(RadioButton)findViewById(R.id.q443);
+        q444=(RadioButton)findViewById(R.id.q444);
+         q445=(RadioButton)findViewById(R.id.q445);
+        q430=(RadioButton)findViewById(R.id.q430);
+        q431=(RadioButton)findViewById(R.id.q431);
+        q432=(RadioButton)findViewById(R.id.q432);
+        q433=(RadioButton)findViewById(R.id.q433);
+        q434=(RadioButton)findViewById(R.id.q434);
+        q435=(RadioButton)findViewById(R.id.q435);
+        q420=(RadioButton)findViewById(R.id.q420);
+        q421=(RadioButton)findViewById(R.id.q421);
+        q422=(RadioButton)findViewById(R.id.q422);
+        q423=(RadioButton)findViewById(R.id.q423);
+        q424=(RadioButton)findViewById(R.id.q424);
+        q425=(RadioButton)findViewById(R.id.q425);
+        q410=(RadioButton)findViewById(R.id.q410);
+        q411=(RadioButton)findViewById(R.id.q411);
+        q412=(RadioButton)findViewById(R.id.q412);
+        q413=(RadioButton)findViewById(R.id.q413);
+        q414=(RadioButton)findViewById(R.id.q414);
+        q415=(RadioButton)findViewById(R.id.q415);
         q45= (SeekBar) findViewById(R.id.q45);
         q46=(SeekBar) findViewById(R.id.q46);
         q47=(EditText) findViewById(R.id.q47);
         q48=(EditText) findViewById(R.id.q48);
+        qq31=(TextView) findViewById(R.id.qq31);
+        qq32=(TextView) findViewById(R.id.qq32);
+        qq33=(TextView) findViewById(R.id.qq33);
+        qq34=(TextView) findViewById(R.id.qq34);
+        qq41=(TextView) findViewById(R.id.qq41);
+        qq42=(TextView) findViewById(R.id.qq42);
+        qq43=(TextView) findViewById(R.id.qq43);
+        qq44=(TextView) findViewById(R.id.qq44);
+        qq45=(TextView) findViewById(R.id.qq45);
+        qq46=(TextView) findViewById(R.id.qq46);
+        qq47=(TextView) findViewById(R.id.qq47);
+        qq48=(TextView) findViewById(R.id.qq48);
         db = FirebaseFirestore.getInstance();
     }
     public void P1(View view) {
@@ -102,21 +179,12 @@ public class Summary_questionnaire_3 extends AppCompatActivity {
 
 
     public void sending(View view) {
-        linearButtons.setVisibility(view.INVISIBLE);
-        scrollView3.setVisibility(view.INVISIBLE);
-        scrollView4.setVisibility(view.INVISIBLE);
+
+    if  (send2()==1) {
         //  FirebaseFirestore.setLoggingEnabled(true);
         Map<String, Object> answers = new HashMap<>();
-        Map<String, Object> part1 = new HashMap<>();
-
-        Map<String, Object> part2 = new HashMap<>();
-
 
         Map<String, Object> part3 = new HashMap<>();
-        Q31=String.valueOf(q31.getRating());
-        Q32=String.valueOf(q32.getRating());
-        Q33=String.valueOf(q33.getRating());
-        Q34=String.valueOf(q34.getRating());
 
         part3.put("q31", Q31);
         part3.put("q32", Q32);
@@ -129,16 +197,13 @@ public class Summary_questionnaire_3 extends AppCompatActivity {
         part4.put("q42", Q42);
         part4.put("q43", Q43);
         part4.put("q44", Q44);
-        int Q45= q45.getProgress();
-        int Q46= q46.getProgress();
+
         part4.put("q45", Q45);
         part4.put("q46", Q46);
-        String Q47 = q47.getText().toString();
-        String Q48 = q48.getText().toString();
+
         part4.put("q47", Q47);
         part4.put("q48", Q48);
-        answers.put("part1", part1);
-        answers.put("part2", part2);
+
         answers.put("part3", part3);
         answers.put("part4", part4);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -160,9 +225,12 @@ public class Summary_questionnaire_3 extends AppCompatActivity {
 
                     }
                 });
-
-
+        linearButtons.setVisibility(view.INVISIBLE);
+        scrollView4.setVisibility(view.INVISIBLE);
         LinearLayout5.setVisibility(view.VISIBLE);
+
+
+    }
     }
     //q1
     public void onRadioButtonClicked1(View view) {
@@ -293,4 +361,81 @@ public class Summary_questionnaire_3 extends AppCompatActivity {
                 break;
         }
     }
+    public int  send2(){
+        Q31=String.valueOf(q31.getRating());
+        Q32=String.valueOf(q32.getRating());
+        Q33=String.valueOf(q33.getRating());
+        Q34=String.valueOf(q34.getRating());
+         Q45 = q45.getProgress();
+         Q46 = q46.getProgress();
+         Q47 = q47.getText().toString();
+         Q48 = q48.getText().toString();
+        if(Objects.equals(Q31, "0.0")) {
+            qq31.setTextColor(getResources().getColor(R.color.design_default_color_error));
+            return 0;
+        }
+        if(Objects.equals(Q32, "0.0")) {
+            qq32.setTextColor(getResources().getColor(R.color.design_default_color_error));
+            return 0;
+        }
+        if(Objects.equals(Q33, "0.0")) {
+            qq33.setTextColor(getResources().getColor(R.color.design_default_color_error));
+            return 0;
+        }
+        if(Objects.equals(Q34, "0.0")) {
+            qq34.setTextColor(getResources().getColor(R.color.design_default_color_error));
+            return 0;
+        }
+        if((!q410.isChecked()) && (!q411.isChecked()) && (!q412.isChecked()) && (!q413.isChecked()) && (!q414.isChecked())&& (!q415.isChecked())) {
+            qq41.setTextColor(getResources().getColor(R.color.design_default_color_error));
+            return 0;
+        }
+        if((!q420.isChecked()) && (!q421.isChecked()) && (!q422.isChecked()) && (!q423.isChecked()) && (!q424.isChecked())&& (!q425.isChecked())) {
+            qq42.setTextColor(getResources().getColor(R.color.design_default_color_error));
+            return 0;
+        }
+        if((!q430.isChecked()) && (!q431.isChecked()) && (!q432.isChecked()) && (!q433.isChecked()) && (!q444.isChecked())&& (!q435.isChecked())) {
+            qq43.setTextColor(getResources().getColor(R.color.design_default_color_error));
+            return 0;
+        }
+        if((!q440.isChecked()) && (!q441.isChecked()) && (!q442.isChecked()) && (!q443.isChecked()) && (!q444.isChecked())&& (!q445.isChecked())) {
+            qq44.setTextColor(getResources().getColor(R.color.design_default_color_error));
+            return 0;
+        }
+
+        if(Q45==0){
+            qq45.setTextColor(getResources().getColor(R.color.design_default_color_error));
+            return 0;
+        }
+        if(Q46==0){
+            qq46.setTextColor(getResources().getColor(R.color.design_default_color_error));
+            return 0;
+
+        }
+        if(Q47.equals("")) {
+            qq47.setTextColor(getResources().getColor(R.color.design_default_color_error));
+            return 0;
+        }
+        if(Q48.equals("")) {
+            qq48.setTextColor(getResources().getColor(R.color.design_default_color_error));
+            return 0;
+        }
+        qq31.setTextColor(getResources().getColor(R.color.black));
+        qq32.setTextColor(getResources().getColor(R.color.black));
+        qq33.setTextColor(getResources().getColor(R.color.black));
+        qq34.setTextColor(getResources().getColor(R.color.black));
+        qq41.setTextColor(getResources().getColor(R.color.black));
+        qq42.setTextColor(getResources().getColor(R.color.black));
+        qq43.setTextColor(getResources().getColor(R.color.black));
+        qq44.setTextColor(getResources().getColor(R.color.black));
+        qq45.setTextColor(getResources().getColor(R.color.black));
+        qq46.setTextColor(getResources().getColor(R.color.black));
+        qq47.setTextColor(getResources().getColor(R.color.black));
+        qq48.setTextColor(getResources().getColor(R.color.black));
+
+
+        return 1;
+    }
+
+
 }
